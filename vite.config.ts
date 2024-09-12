6966d6b329ce9d25ae6f9f4b845e14205de89952
John Mike <mikealvizo44@gmail.com>
2024-09-13 01:44:43 +0800

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/numoa-olivr/",
+  base:"/numoa-olivr",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: () => "everything.js",
+      },
     },
   },
 });
