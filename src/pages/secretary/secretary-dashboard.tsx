b49cf0d47153973040fd499491c9fb4723ba3118
John Mike <mikealvizo44@gmail.com>
2024-09-13 02:41:15 +0800

    updatedd sec dashboard

--- src/pages/secretary/secretary-dashboard.tsx
+++ src/pages/secretary/secretary-dashboard.tsx
@@ -3,12 +3,14 @@ import { title } from "../../components/primitives"
 import { AccountsPieChart } from "@/components/charts/AccountsPieChart"
 import { PatientsBarChart } from "@/components/charts/PatientsBarChart"
 import BarChartInterActive from "@/components/charts/BarChartInterActive"
+import { Link } from "react-router-dom"
 
 const SecretaryDashboard = () => {
     return (
         <section className="flex flex-1 flex-col items-start justify-start gap-4 px-8 w-full">
             <div className="inline-block max-w-lg text-center justify-center">
                 <h1 className={title({ size: "sm" })}>Dashboard</h1>
+                <Link to={"/secretary"}>Go to secretary</Link>
             </div>
             <div className="flex flex-row w-full h-fit max-w-full justify-evenly gap-4">
                 <LinearChart />
@@ -17,10 +19,6 @@ const SecretaryDashboard = () => {
             <div className="w-full h-fit flex flex-row gap-4">
                 <AccountsPieChart />
                 <BarChartInterActive />
-            </div>
-            <div className="w-full h-fit flex flex-row gap-4">
-                <AccountsPieChart />
-                <LinearChart />
             </div>
         </section>
     )
